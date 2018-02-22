36fc61cce98e35c817fca66816944426f055502a
Isaac Datlof <ike18t@gmail.com>
2018-02-21 20:18:09 -0500

    fix: export flipper service

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { Flip, Flipadelphia } from './src/flipadelphia';
 export { FlipadelphiaComponent } from './src/flipadelphia.component';
 export { FlipadelphiaModule } from './src/flipadelphia.module';
+export { FlipperService } from './src/flipper.service';
 export { LocalStorageFlipperService } from './src/local-storage-flipper.service';
